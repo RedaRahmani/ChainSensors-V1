7c9ed16036dabe3a65eecbf52cc599aba1c6e4e6
RedaRahmani <mohamedreda.rahmani20@ump.ac.ma>
2025-05-06 14:03:37 +0100

    intialize and device registry contracts done

---
+++ chain-sensors-contracts/programs/chain-sensors-contracts/src/instructions/mod.rs
@@ -0,0 +1,7 @@
+pub mod initialize;
+
+pub use initialize::*;
+
+pub mod device_registry;
+
+pub use device_registry::*;
---
+++ chain-sensors-contracts/programs/chain-sensors-contracts/src/state/mod.rs
@@ -0,0 +1,7 @@
+pub mod marketplace;
+
+pub use marketplace::*;
+
+pub mod device_registry;
+
+pub use device_registry::*;
